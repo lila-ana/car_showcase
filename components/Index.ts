227afa1e4e66284930fe8fda7c66a385c1127cd2
lila-ana <gelilat1216@gmail.com>
2024-04-26 11:34:29 +0300

    pushing after the serach has been changed to client side rendering

---
+++ components/Index.ts
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomButton from "./CustomButton";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  Navbar,
+  Footer,
+  CustomButton,
+  CustomFilter,
+  SearchBar,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+};
---
+++ components/SearchBar.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { ChangeEvent, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import SearchManufacturer from "./SearchManufacturer";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
+const SearchBar: React.FC = ({ setModel, setManufacturer }) => {
+  const [searchManufacturer, setSearchManufacturer] = useState<string>("");
+  const [searchModel, setSearchModel] = useState<string>("");
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (searchManufacturer === " " && searchModel === " ") {
+      return alert("Please fill in the search bar");
+    }
+    setModel(searchModel);
+    setManufacturer(searchManufacturer);
+  };
+
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          selected={searchManufacturer}
+          setSelected={setSearchManufacturer}
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4 "
+          alt="car searchModel"
+        />
+        <input
+          type="text"
+          name="searchModel"
+          value={searchModel}
+          placeholder="Tiguan..."
+          className="searchbar__input"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setSearchModel(e.target.value)
+          }
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
+    </form>
+  );
+};
+
+export default SearchBar;
